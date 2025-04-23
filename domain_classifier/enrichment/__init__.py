31a9ff70a6c54d81f345890ef1425059bb2a6465
olibott123 <87874530+olibott123@users.noreply.github.com>
2025-04-23 10:47:31 +0100

    Update __init__.py

---
+++ domain_classifier/enrichment/__init__.py
@@ -0,0 +1,6 @@
+"""Enrichment module for domain classifier.
+This module adds additional company data to classification results.
+"""
+# Make key components available at the package level
+from domain_classifier.enrichment.apollo_connector import ApolloConnector
+from domain_classifier.enrichment.recommendation_engine import DomotzRecommendationEngine
