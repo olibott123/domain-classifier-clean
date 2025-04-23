d87c052405fa4dfddb9f621b1174311aadf21f51
olibott123 <87874530+olibott123@users.noreply.github.com>
2025-04-23 11:35:22 +0100

    Update __init__.py

--- domain_classifier/enrichment/__init__.py
+++ domain_classifier/enrichment/__init__.py
@@ -4,3 +4,4 @@ This module adds additional company data to classification results.
 # Make key components available at the package level
 from domain_classifier.enrichment.apollo_connector import ApolloConnector
 from domain_classifier.enrichment.recommendation_engine import DomotzRecommendationEngine
+from domain_classifier.enrichment.description_enhancer import enhance_company_description, generate_detailed_description
