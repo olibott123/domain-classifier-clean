b431e5409e5df60e4ba3ae64fbedc7db8c14fcd8
olibott123 <87874530+olibott123@users.noreply.github.com>
2025-04-29 10:14:49 +0100

    Update __init__.py

--- domain_classifier/utils/__init__.py
+++ domain_classifier/utils/__init__.py
@@ -5,5 +5,6 @@ This module contains utility functions used across the domain classification sys
 
 # Make key utility functions available at the package level
 from domain_classifier.utils.domain_utils import extract_domain_from_email
-from domain_classifier.utils.error_handling import detect_error_type, create_error_result
+from domain_classifier.utils.error_handling import detect_error_type, create_error_result, check_domain_dns
 from domain_classifier.utils.text_processing import extract_json, clean_json_string
+from domain_classifier.utils.final_classification import determine_final_classification
