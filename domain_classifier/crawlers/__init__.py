c87a003f2dd3049ed89bdae6d6a1568f4070b8b9
olibott123 <87874530+olibott123@users.noreply.github.com>
2025-04-30 11:06:26 +0100

    Update __init__.py

--- domain_classifier/crawlers/__init__.py
+++ domain_classifier/crawlers/__init__.py
@@ -3,6 +3,6 @@
 This module contains website content crawlers that extract text from domains.
 """
 
-# Make crawler functions available at package level
-from domain_classifier.crawlers.apify_crawler import crawl_website
+# Make the function directly available at this level
+from domain_classifier.crawlers.apify_crawler import crawl_website, detect_error_type
 from domain_classifier.crawlers.direct_crawler import direct_crawl
